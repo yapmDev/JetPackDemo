1a8fd303f611e0152bdb918f2f18bde361659c12
Yosmel Pérez <ronnynordelo@gmail.com>
2024-02-15 21:14:31 -0500

    proyect is ready

---
+++ app/src/main/java/com/example/jetpackdemo/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.jetpackdemo
+
+import android.content.res.Configuration
+import android.content.res.Resources.Theme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackdemo.ui.theme.JetPackDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Dear")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetPackDemoTheme {
+        Greeting("Android")
+    }
+}
